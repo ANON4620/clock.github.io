691fa0ee9b09fdfb76f83985b4a20f8bbf2e1701
ANON4620 <swarnadeep.click@gmail.com>
2021-05-30 19:21:47 +0530

    Updated to 12 hour format, Bug fixes

--- main.js
+++ main.js
@@ -3,9 +3,16 @@ const clock = document.getElementById('clock');
 function updateTime() {
   const time = new Date();
 
-  const h = time.getHours();
-  const m = time.getMinutes();
-  const s = time.getSeconds();
+  let h = time.getHours();
+  let m = time.getMinutes();
+  let s = time.getSeconds();
+  if(h > 12)
+    h = h - 12;
+    
+  if(h < 10)
+    h = `0${h}`;
+  if(m < 10)
+    m = `0${m}`;
   if(s < 10)
     s = `0${s}`;
 
